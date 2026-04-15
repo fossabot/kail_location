c0cd8a9a6cff35e42c3f4063ab7e272ca48b76de
noellegazelle6 <noellegazelle6@gmail.com>
2026-04-15 18:03:25 +0800

    Update ShellUtils.kt

--- app/src/main/java/com/kail/location/utils/ShellUtils.kt
+++ app/src/main/java/com/kail/location/utils/ShellUtils.kt
@@ -17,29 +17,22 @@ object ShellUtils {
     }
 
     fun executeCommand(command: String): String {
-        KailLog.i(null, "ShellUtils", ">>> executeCommand: $command")
-        return try {
-            val process = Runtime.getRuntime().exec(arrayOf("sh", "-c", command))
-
-            val result = process.inputStream.bufferedReader().readText()
-            val error = process.errorStream.bufferedReader().readText()
-
+        val runtime = Runtime.getRuntime()
+        try {
+            val process = runtime.exec("su")
+            process.outputStream.write("$command\n".toByteArray())
+            process.outputStream.write("exit\n".toByteArray())
+            process.outputStream.flush()
             process.waitFor()
-
             if (process.exitValue() != 0) {
-                KailLog.e(null, "ShellUtils", ">>> command failed: $command, error: $error")
-                error
-            } else {
-                KailLog.i(null, "ShellUtils", ">>> command succeeded: $command, result: $result")
-                result
+                return process.errorStream.bufferedReader().readText()
             }
+            return process.inputStream.bufferedReader().readText()
         } catch (e: Exception) {
-            KailLog.e(null, "ShellUtils", ">>> command exception: $command, error: ${e.message}")
             e.printStackTrace()
-            ""
+            return ""
         }
     }
-
 
     fun executeCommandToBytes(command: String): ByteArray {
         val runtime = Runtime.getRuntime()
